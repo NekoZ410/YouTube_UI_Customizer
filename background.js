f5e6431137801779ab69b236f1777543498e6fec
NekoZ410 <1chumeocon@gmail.com>
2025-09-05 09:18:46 +0700

    background.js: refactor some code

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 // background script
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === "youtube_action") {
-        console.log("YouTube action...");
-        sendResponse({ status: "YouTube action received in background script" });
-    }
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     if (request.message === "youtube_action") {
+//         console.log("YouTube action...");
+//         sendResponse({ status: "YouTube action received in background script" });
+//     }
 
-    return false;
-});
+//     return false;
+// });
